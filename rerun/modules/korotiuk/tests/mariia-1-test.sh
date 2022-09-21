833012e491ca37dd4a088b4862af1f25a0653eac
Masha_Korotiuk <mariakorotiuk@gmail.com>
2022-09-21 21:33:07 +0300

    task1 rerun

---
+++ rerun/modules/korotiuk/tests/mariia-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m korotiuk -p mariia [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "mariia"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
